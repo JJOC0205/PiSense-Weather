0d74513076f4e2c265180f29220c264c77385761
Jon O'Connell <97076489+JJOC0205@users.noreply.github.com>
2022-07-22 23:24:01 -0400

    Base Code

---
+++ weather_script.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+from sense_hat import SenseHat
+import time
+import sys
+from ISStreamer.Streamer import Streamer
+BUCKET_NAME = 'PiWeather'
+BUCKET_KEY = 'JUTWG4L4ELY6'
+ACCESS_KEY = 'ist_PEK-0lH8XL8aAzJnyfEhee3TunlLxBYM'
+sense = SenseHat()
+logger=Streamer(bucket_name=BUCKET_NAME,bucket_key = BUCKET_KEY,access_key=ACCESS_KEY)
+sense.clear()
+try:
+    while True:
+        temp = sense.get_temperature()
+        temp = 1.8 * round(temp,1) + 32
+        temp = round(temp,1)
+        logger.log("Temperature F",temp)
+
+        hummidity = sense.get_humidity()
+        humidity = round(hummidity,1)
+        logger.log("Humidiity :",humidity)
+
+        pressure = sense.get_pressure()
+        pressure = round(pressure,1)
+        logger.log("Pressure: ",pressure)
+
+        time.sleep(1)
+except KeyboardInterrupt:
+    pass

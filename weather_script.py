fe9f5805f2da21e721c90419085b979e63b8e494
Jon O'Connell <jjoc0205@gmail.com>
2024-12-06 21:46:39 -0500

    testing hat

--- weather_script.py
+++ weather_script.py
@@ -2,28 +2,23 @@
 from sense_hat import SenseHat
 import time
 import sys
-from ISStreamer.Streamer import Streamer
-BUCKET_NAME = 'Bucket Name Here'
-BUCKET_KEY = 'Bucket Key Here'
-ACCESS_KEY = 'Access Key Here'
+
 sense = SenseHat()
-logger=Streamer(bucket_name=BUCKET_NAME,bucket_key = BUCKET_KEY,access_key=ACCESS_KEY)
-sense.clear()
-try:
+
+def main():
     while True:
-        temp = sense.get_temperature()
-        temp = 1.8 * round(temp,1) + 32
-        temp = round(temp,1)
-        logger.log("Temperature F",temp)
+        command = input("Temperature, Humidity, or Exit:")
+        if command == "Temperature":
+            print("Temperature: %s C" % sense.get_temperature())
+            sense.show_message("Temperature: %s C" % sense.get_temperature())
+        elif command == "Humidity":
+            print("Humidity: %s %%rH" % sense.get_humidity())
+            sense.show_message("Humidity: %s %%rH" % sense.get_humidity())
+        elif command == "Exit":
+            sys.exit()
+        else:
+            print("Invalid command")
 
-        hummidity = sense.get_humidity()
-        humidity = round(hummidity,1)
-        logger.log("Humidiity :",humidity)
 
-        pressure = sense.get_pressure()
-        pressure = round(pressure,1)
-        logger.log("Pressure: ",pressure)
-
-        time.sleep(1)
-except KeyboardInterrupt:
-    pass
+if __name == "main":
+    main()
